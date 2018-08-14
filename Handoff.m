00c6d7bb1e5706c570d21e2d584bc2566045db6f
leticiacoelho <leticia.ac23@gmail.com>
2018-08-14 09:03:21 -0300

    Handoff

--- Handoff.m
+++ Handoff.m
@@ -1,37 +1,31 @@
 % RAPPAPORT, Theodore S. - Comunicações Sem Fio Princípios e Práticas; 1a ed. - Exercício 4.25 - PG. 111
 % Desenvolvido por Jessica hahn (https://github.com/jessicasilvahahn) e modificado por Leticia Coelho.
 
-close all;
-clear all;
-n = 4
-desvio = 6
-d0 = 1
-P0 = 0
-D = 1600
-d1 = 1:1:D
-d2 = D - d1
-Ph0 = -112
-Pr_min = -118
-Pr1 = P0 -10*n*log10(d1./d0);
-Pr2 = P0 -10*n*log10(d2./d0);
-figure()
-plot(Pr1)
-hold on
-plot(Pr2)
-hold off;
+close all;clear all;clc;
+n = 4;
+sigma = 6; % Desvio padrão
+d0 = 1;
+P0 = 0; % Potência média
+D = 1600;
+d1 = 1:1:D;
+d2 = D - d1;
+Ph0 = -112; % dBm
+Pr_min = -118; % dBm
 
-%probabilidades
-Prb1 = qfunc((Pr1 - (Ph0))./desvio);
-Prb2 = qfunc(((Ph0)-Pr2)./desvio);
+% Modelo de propagação em larga escala - Potencia cai até encontrar o limiar
+% de Handoff, então inicia a transferência para a próxima Estação Base (Eb).
 
-figure()
-plot(Prb1)
-hold on
-plot(Prb2)
-hold off;
+Pr1 = P0 -10*n*log10(d1./d0); % Potência recebida - Eb1
+Pr2 = P0 -10*n*log10(d2./d0); % Potência recebida - Eb2
 
-%probabilidade total (seguindo teorema estatistico)
-Ptot = Prb1.*Prb2;
+% Ponto de Handoff é o limiar de troca de Eb.
+figure(1)
+plot(Pr1);hold on;plot(Pr2);text(810,-116,'Handoff');plot(802,-116,'rs');hold off;legend('Pot - Eb1','Pot - Eb2');title('Potência recebida');
 
-figure()
-plot(Ptot)
+% Probabilidades
+Prb1 = qfunc((Pr1 - (Ph0))./sigma); % Pr1 < Ho
+Prb2 = qfunc(((Ph0)-Pr2)./sigma); % Pr2 > Min
+Ptot = Prb1.*Prb2; % Handoff acontecer
+
+figure(2)
+plot(Prb1);hold on;plot(Prb2);plot(Ptot);hold off;legend('Pr[Pt1 < Ho]','Pr[Pt2 > Min]','Pr[Ho]');title('Probabilidades');
